1551d0e5e55fa869f7e2042166d9ebc43b6c10f1
Nemikolh <joan.varvenne@gmail.com>
2017-11-02 14:45:20 +0000

    Limit clone depth to 1.

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-git clone git@github.com:facebook/yoga.git tmp
+git clone git@github.com:facebook/yoga.git tmp --depth 1
 cp tmp/yoga/* c/
 rm -rf tmp
 bindgen --no-unstable-rust --whitelist-function "^YG.*" c/wrapper.h -o src/ffi.rs
